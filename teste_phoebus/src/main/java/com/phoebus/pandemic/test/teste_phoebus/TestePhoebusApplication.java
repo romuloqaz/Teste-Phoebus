2a429af578fa3fc93668b8d59d5d516146355404
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-11-10 06:18:42 -0300

    first commit

---
+++ teste_phoebus/src/main/java/com/phoebus/pandemic/test/teste_phoebus/TestePhoebusApplication.java
@@ -0,0 +1,13 @@
+package com.phoebus.pandemic.test.teste_phoebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePhoebusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePhoebusApplication.class, args);
+	}
+
+}
---
+++ teste_phoebus/src/test/java/com/phoebus/pandemic/test/teste_phoebus/TestePhoebusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoebus.pandemic.test.teste_phoebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePhoebusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
